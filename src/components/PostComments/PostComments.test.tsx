99e2258fea00d12c9f79597de8631e04cc99b00b
WiliamDev <wiliam.omura3@hotmail.com>
2025-05-13 12:52:32 -0300

    1.1
    Com apenas 1 warning mais o teste ele passou

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,22 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComments from '../Post'; 
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+it('Deve adicionar dois comentários corretamente', () => {
+    render(<PostComments/>);
+
+    const input = screen.getByTestId('comment-input');
+    const button = screen.getByTestId('submit-button');
+
+    
+    fireEvent.change(input, { target: { value: 'Primeiro comentário' } });
+    fireEvent.click(button);
+
+
+    fireEvent.change(input, { target: { value: 'Segundo comentário' } });
+    fireEvent.click(button);
+
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+    expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
+});
 });
